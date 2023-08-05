c2674f0663d7d44fa3cf0854581e4c6740489e8e
Paulo19961944 <106405308+Paulo19961944@users.noreply.github.com>
2023-08-05 16:34:03 -0300

    Update Main.java

--- src/br/senai/sp/form/Main.java
+++ src/br/senai/sp/form/Main.java
@@ -1,7 +1,6 @@
 package br.senai.sp.form;
 public class Main {
     public static void main(String[] args) {
-
         /** Faz a instancia da classe form */
         Form prof1 = new Form();
         /** Chama a instancia */
